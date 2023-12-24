37b601e7018acad831b8bc6ece3bb503b1e9cd72
Rishi Kambil <rishikambil@gmail.com>
2023-12-24 11:17:57 +0530

    refactor: split into a different function

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,42 @@
+// mod datatypes;
+
+use core::num::ParseIntError;
 use rand::{thread_rng, Rng};
-use std::cmp::Ordering;
-use std::io;
+use std::{cmp::Ordering, io};
+
+fn user_input() -> Result<u32, ParseIntError> {
+    println!("Please input your guess.");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    match guess.trim().parse() {
+        Ok(num) => Ok(num),
+        Err(e) => Err(e),
+    }
+}
 fn main() {
     println!("Guess the number!");
 
     loop {
-        println!("Please input your guess.");
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let current: u32 = thread_rng().gen_range(0..11);
+        match user_input() {
+            Ok(guess) => {
+                match guess.cmp(&current) {
+                    Ordering::Less => println!("Aim Higher!"),
+                    Ordering::Equal => {
+                        println!("Correct guess! Exiting game!");
+                        break;
+                    }
+                    Ordering::Greater => println!("Aim lower!"),
+                }
+                println!("Current = {current}");
+            }
             Err(_) => {
-                println!("invalid input, please enter only numbers!");
                 continue;
             }
-        };
-
-        let current: u32 = thread_rng().gen_range(0..11);
-
-        match guess.cmp(&current) {
-            Ordering::Less => println!("Aim Higher!"),
-            Ordering::Equal => {
-                println!("Correct guess! Exiting game!");
-                break;
-            }
-            Ordering::Greater => println!("Aim lower!"),
         }
-        println!("Current = {current}");
     }
 }
